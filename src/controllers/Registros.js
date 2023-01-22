c204e1bbc417ec98ad98f5c43448aa53a7751df4
Laura <laurasouzacastro1@gmail.com>
2023-01-22 16:29:20 -0300

    valor do saldo

--- src/controllers/Registros.js
+++ src/controllers/Registros.js
@@ -1,10 +1,18 @@
 import db from '../config/database.js';
-
+import dayjs from 'dayjs';
 export async function listaRegistros(req, res) {
     try {
         const logado = res.locals.sessao;
         const registros = await db.collection("registros").find({idUsuario: logado.idUsuario}).toArray();
-        return res.send(registros);
+        let saldo = 0;
+        for(let i = 0; i < registros.length; i++){
+            if(registros[i].tipo === 'saida'){
+                saldo -= Number(registros[i].valor);
+            }else if(registros[i].tipo === 'entrada'){
+                saldo += Number(registros[i].valor);
+            }
+        }
+        return res.send({registros: registros.reverse(), saldo: saldo.toFixed(2)});
     } catch (error) {
         res.status(500).send("Erro no servidor");
     }
@@ -14,7 +22,7 @@ export async function criaRegistros(req, res) {
     try {
         const registro = req.body;
         const logado = res.locals.sessao;
-        await db.collection("registros").insertOne({ valor: registro.valor, descricao: registro.descricao, tipo: registro.tipo, idUsuario: logado.idUsuario });
+        await db.collection("registros").insertOne({ valor: registro.valor, descricao: registro.descricao, tipo: registro.tipo, data: `${dayjs().format('DD/MM')}`, idUsuario: logado.idUsuario });
         res.sendStatus(201);
     } catch (error) {
       res.status(500).send("Erro no servidor");
